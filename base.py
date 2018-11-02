bd3e6eeff90f5234f033d9f5c1c45bc2706c370a
Goro <goro@iMac.local>
2018-11-02 11:00:49 -0400

    local commit

--- base.py
+++ base.py
@@ -1,3 +1,19 @@
 """
 Base module for the encryption/decryption code
+With another change
 """
+
+def upstream_commit():
+	"""
+	test upstream commit
+	"""
+	print('does this work?')
+
+	pass
+
+
+upstream_commit()
+sys.exit()
+
+# comment this out
+# upstream_commit()
